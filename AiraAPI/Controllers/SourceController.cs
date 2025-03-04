bb92646de46ce977ab87b5f3d1e062a8edf2d2fb
PNY92 <pxitrbx@gmail.com>
2025-03-04 11:50:03 +0800

    Added some changes to ConfigManager and implemented config.json for developers

---
+++ AiraAPI/Controllers/SourceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AiraAPI.Controllers
+{
+    public class SourceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- AiraAPI/Repositories/ConfigManager.cs
+++ AiraAPI/Repositories/ConfigManager.cs
@@ -6,11 +6,20 @@ namespace AiraAPI.Repositories
 {
     public static class ConfigManager
     {
+        private const string _filePath_dev = "config-dev.json";
         private const string _filePath = "config.json";
 
         public static JObject GetConfiguration()
         {
-            string filePath = _filePath;
+            string filePath = String.Empty;
+            if (Path.Exists("config-dev.json"))
+            {
+                filePath = _filePath_dev;
+            }
+            else
+            {
+                filePath = _filePath;
+            }
             string jsonString = File.ReadAllText(filePath);
             JObject? jObject = JsonConvert.DeserializeObject<dynamic>(jsonString);
 
